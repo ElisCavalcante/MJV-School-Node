3be86997a78f96112887cca79949ebefeef12fdd
ElisCavalcante <elisabeth.rsc@gmail.com>
2023-03-25 18:21:25 -0300

    exercicio 2 - segunda semana

---
+++ MJV-School-Node-Segunda-Semana/primeiro-arquivo-node-js/hello-world.js
@@ -0,0 +1,10 @@
+console.log("Hello world ! :D ");
+
+const a = 5
+const b = 2
+
+const soma  = () =>{
+    return a + b
+}
+
+console.log("O resultado da soma é: " , soma())
---
+++ MJV-School-Node-Segunda-Semana/primeiro-projeto/primeiro-arquivo.ts
@@ -0,0 +1,10 @@
+console.log("Hello Typescript! :D ")
+
+let a = 5
+let b = 3
+
+let soma = a + b
+
+let texto1 = "Deu certo! :D"
+
+console.log( "O resultado é :", soma , texto1)
